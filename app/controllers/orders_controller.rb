9e39e946ee2aa617535eae2d4a12e1becca40a5e
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-11-29 06:49:10 +0000

    fix: Remove devise-guests gem

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
   before_action :set_order, only: %i[show edit update destroy]
 
   # GET /orders
