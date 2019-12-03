731635a4d2cc15be811d5f9005841b2f453bbc94
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-12-03 09:55:15 +0000

    feat: Show recently bought products on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @product = Product.new
+    @products = Product.joins(:orders)
+                    .where(orders: { status: :success })
+                    .order(created_at: :desc)
+                    .limit(6)
   end
 end
