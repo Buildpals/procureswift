aaa12fa9f97f013811127f4b56abdc223e93cfe8
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-12-03 20:10:35 +0000

    feat: Put an add new product form at the bottom of products#index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all.order(created_at: :desc)
+    @product = Product.new
   end
 
   # GET /products/1
