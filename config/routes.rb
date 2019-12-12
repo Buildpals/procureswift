4830801cc0c40e818360adcc4cbfd0dd5693fe68
bkwaku <kbboateng14@gmail.com>
2019-12-12 12:30:56 +0000

    Scale footer down to infomation we have now

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,6 @@ Rails.application.routes.draw do
   get '/privacy_policy', to: 'welcome#privacy_policy', as: :privacy_policy
   get '/terms', to: 'welcome#terms', as: :terms_and_conditions
   get '/refund_policy', to: 'welcome#refund_policy', as: :refund_policy
-  get '/career', to: 'welcome#career', as: :career
+  # get '/career', to: 'welcome#careers', as: :careers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
