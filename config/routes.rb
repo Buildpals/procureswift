b81c2043303daec8f5bc7f5b70f58490a0da44da
Peter O'Hara Adu <ohara.invent@gmail.com>
2020-01-21 14:26:43 +0000

    feat: Add RailsAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
   namespace :admin do
     resources :products
     resources :carts
