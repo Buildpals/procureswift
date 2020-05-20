208eb4b8e2251a842a5dbb36b359b4c96da96167
bkwaku <kbboateng14@gmail.com>
2020-05-20 11:10:54 +0100

    change address to official warehouse address

--- app/services/order_builder.rb
+++ app/services/order_builder.rb
@@ -1,25 +1,37 @@
 class OrderBuilder
-  US_ADDRESS = {    
-    "first_name": "Tim",  
-    "last_name": "Beaver",  
-    "address_line1": "77 Massachusetts Avenue",  
-    "address_line2": "",  
-    "zip_code": "02139",  
-    "city": "Cambridge",  
-    "state": "MA",  
-    "country": "US",  
-    "phone_number": "5551230101" 
+  US_SHIPPING_ADDRESS = {
+    "first_name": "ProcureSwift",
+    "last_name": "MyUS",
+    "address_line1": "4283 Express Lane", 
+    "address_line2": "Suite 5758-751",
+    "zip_code": "34249",
+    "city": "Sarasota",
+    "state": "FL",
+    "country": "US",
+    "phone_number": "(941) 538-6941"
   }
-  UK_ADDRESSS = {    
-    "first_name": "Tim",  
-    "last_name": "Beaver",  
-    "address_line1": "77 Massachusetts Avenue",  
-    "address_line2": "",  
-    "zip_code": "02139",  
-    "city": "Cambridge",  
-    "state": "MA",  
-    "country": "US",  
-    "phone_number": "5551230101" 
+  UK_SHIPPING_ADDRESSS = {
+    "first_name": "Tim",
+    "last_name": "Beaver",
+    "address_line1": "77 Massachusetts Avenue",
+    "address_line2": "",
+    "zip_code": "02139",
+    "city": "Cambridge",
+    "state": "MA",
+    "country": "US",
+    "phone_number": "5551230101"
+  }
+
+  BILLING_ADDRESS = {
+    "first_name": "Web Services -",
+    "last_name": " ProcureSwift",
+    "address_line1": "333 Fremont Street", 
+    "address_line2": "",
+    "zip_code": "94105",
+    "city": "San Francisco",
+    "state": "CA",
+    "country": "US",
+    "phone_number": ""
   }
 
   def initialize(order)
@@ -29,47 +41,25 @@ class OrderBuilder
 
   def get_request_body
     static_body = { 
-      "max_price": @shipping_info.cost / ProductBuilder::CENTS_TO_DOLLARS_RATIO, 
-      "shipping_address": {    
-                            "first_name": "Tim",  
-                            "last_name": "Beaver",  
-                            "address_line1": "77 Massachusetts Avenue",  
-                            "address_line2": "",  
-                            "zip_code": "02139",  
-                            "city": "Cambridge",  
-                            "state": "MA",  
-                            "country": "US",  
-                            "phone_number": "5551230101" 
-                          }, 
+      "max_price": @shipping_info.cost / ProductBuilder::CENTS_TO_DOLLARS_RATIO,
        "is_gift": true, 
-       "gift_message": "Here is your package, Tim! Enjoy!", 
+       "gift_message": "Here is your package, Tim! Enjoy!",
        "shipping": {  
-                        "order_by": "price",  
-                        "max_days": 5,  
-                        "max_price": 1000 
+                        "order_by": "price",
+                        "max_days": 5,
+                        "max_price": 1000
                       }, 
       "payment_method": {  
-                            "name_on_card": "Ben Bitdiddle",  
-                            "number": "5555555555554444",  
-                            "security_code": "123",  
-                            "expiration_month": 1,  
-                            "expiration_year": 2020,  
-                            "use_gift": false 
-                          }, 
-      "billing_address": {   
-                             "first_name": "William",  
-                              "last_name": "Rogers",  
-                              "address_line1": "84 Massachusetts Ave",  
-                              "address_line2": "",  
-                              "zip_code": "02139",  
-                              "city": "Cambridge",  
-                              "state": "MA",  
-                              "country": "US",  
-                              "phone_number": "5551234567" 
-                            }, 
+                            "name_on_card": "Web Services - ProcureSwift",
+                            "number": "4056401952976392",
+                            "security_code": "345",
+                            "expiration_month": 2,
+                            "expiration_year": 2023,
+                            "use_gift": false
+                          },
    "retailer_credentials": {   
-                               "email": "timbeaver@gmail.com",  
-                               "password": "myRetailerPassword",  
+                               "email": "timbeaver@gmail.com",
+                               "password": "myRetailerPassword",
                                "totp_2fa_key": "3DE4 3ERE 23WE WIKJ GRSQ VOBG CO3D METM 2NO2 OGUX Z7U4 DP2H UYMA" 
                               }, 
    "webhooks": {   
@@ -78,8 +68,8 @@ class OrderBuilder
                   "tracking_obtained": "http://mywebsite.com/zinc/tracking_obtained" 
                  }, 
    "client_notes": {  
-                        "our_internal_order_id": "abc123",  
-                        "any_other_field": ["any value"]  
+                        "our_internal_order_id": "abc123",
+                        "any_other_field": ["any value"]
                       } 
     }
 
@@ -94,15 +84,15 @@ class OrderBuilder
     amazon_order_hash = {
       "retailer": "amazon",
       "products": amazon_products,
-      "shipping_address": US_ADDRESS,
-      "billing_address": US_ADDRESS
+      "shipping_address": US_SHIPPING_ADDRESS,
+      "billing_address": BILLING_ADDRESS
 
     }
     amazon_uk_order_hash = {
       "retailer": "amazon_uk",
       "products": amazon_uk_products,
-      "shipping_address": UK_ADDRESSS,
-      "billing_address": UK_ADDRESSS
+      "shipping_address": UK_SHIPPING_ADDRESSS,
+      "billing_address": BILLING_ADDRESS
     }
     amazon_order_hash.merge!(static_body)
     amazon_uk_order_hash.merge!(static_body)
